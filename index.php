ae39ee0357c4cec12631e62b98495de368b7fb16
SpectralX <galaxies20122@gmail.com>
2021-02-01 19:24:42 +0100

    Időjárás kész

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carlo</title>
+</head>
+<body>
+
+<!-- ournet.hu Weather Widget -->
+<iframe src="//idojaras.ournet.hu/widget/widget_frame?id=3054643&days=5&w=200" scrolling="no" frameborder="0" style="border:none;overflow:hidden;height:243px;width:200px;" allowTransparency="true"></iframe>
+<noscript><a href="http://idojaras.ournet.hu">ournet.hu</a></noscript>
+
+</body>
+</html>
