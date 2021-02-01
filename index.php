163b0c6f95530a2a7d0383c58bf68db679cb4368
frucci44 <sallaifruzsina44@gmail.com>
2021-02-01 14:00:39 +0100

    ujmenu

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-4.6.0-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="lovi.css" type="text/css">
+    <title>Carlo Lovarda</title>
+</head>
+<body>
+<?php include 'menu.php'; ?>
+<h1>Egy ló, Egy élet, egy szerelem!</h1>
+    
+</body>
+</html>
