9431dc646825734c46d5a2f4077a863e13106f23
SpectralX <galaxies20122@gmail.com>
2021-02-01 19:28:53 +0100

    Időjárás kész

--- php/weather.php
+++ php/weather.php
@@ -1,12 +1,9 @@
-<a class="weatherwidget-io" href="https://forecast7.com/hu/47d3919d12/district-xxiii/" data-label_1="Soroksár" data-label_2="Carlo Lovassport Egyesület" data-days="3" data-theme="gray" >Soroksár Carlo Lovassport Egyesület</a>
+<a class="weatherwidget-io" href="https://forecast7.com/hu/47d3919d12/district-xxiii/" data-label_1="Soroksár" data-label_2="Carlo Lovassport Egyesület" data-font="Roboto" data-icons="Climacons Animated" data-days="5" data-theme="gray" >Soroksár Carlo Lovassport Egyesület</a>
 <script>
 !function(d,s,id){
-    var js,
-    fjs=d.getElementsByTagName(s)[0];
-    if(!d.getElementById(id)){
-        js=d.createElement(s);
-        js.id=id;
-        js.src='https://weatherwidget.io/js/widget.min.js';
-        fjs.parentNode.insertBefore(js,fjs);}}
-        (document,'script','weatherwidget-io-js');
+    var js,fjs=d.getElementsByTagName(s)[0];
+    if(!d.getElementById(id)){js=d.createElement(s);
+    js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+    fjs.parentNode.insertBefore(js,fjs);}}
+    (document,'script','weatherwidget-io-js');
 </script>
