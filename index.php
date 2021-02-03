4d77042dfbe76bb3e607ad0fe46d88d359c54f22
SpectralX <galaxies20122@gmail.com>
2021-02-03 09:17:03 +0100

    Időjárás fix

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<!-- ournet.hu Weather Widget -->
+<!-- Időjárás applikáció alulra -->
 <iframe src="//idojaras.ournet.hu/widget/widget_frame?id=3054643&days=5&w=200" scrolling="no" frameborder="0" style="border:none;overflow:hidden;height:243px;width:200px;" allowTransparency="true"></iframe>
 <noscript><a href="http://idojaras.ournet.hu">ournet.hu</a></noscript>
 
