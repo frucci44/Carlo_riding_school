23aa73551f94af5f54a9527b10825476e61d019c
SpectralX <galaxies20122@gmail.com>
2021-02-03 09:12:47 +0100

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html lang="hu" dir="ltr">
+<html lang="hu">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=width-device-widthm initial-scale=1.0">
     <title>Kapcsolat</title>
   </head>
   <body>
