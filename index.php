24b14f58484e8a7f74824f3a50190a0af49a0a52
SpectralX <galaxies20122@gmail.com>
2021-02-04 19:17:02 +0100

    Toggle menu WORKING

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="bootstrap-4.6.0-dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/custom.css" type="text/css">
     <title>Carlo Lovarda</title>
 </head>
