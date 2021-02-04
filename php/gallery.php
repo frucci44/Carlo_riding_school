8ce9b98d1567fb29a3676d31c9715abbab25c545
SpectralX <galaxies20122@gmail.com>
2021-02-04 18:00:16 +0100

    Update menu, gallery stb..

---
+++ php/gallery.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/gallery.css">
+    <title>Galéria</title>
+</head>
+
+<body>
+    <?php include 'menu.php'; ?>
+    <div class="container">
+        <div class="carousel usztatas">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</body>
+
+</html>
