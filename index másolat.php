aecbf37a96d99d98ef640cfc4d683c0fded6dacc
atesz77 <34383667+atesz77@users.noreply.github.com>
2021-02-07 13:32:26 +0100

    teszt

---
+++ index másolat.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-4.6.0-dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/custom.css" type="text/css">
+    <title>Carlo Lovarda</title>
+</head>
+
+<body>
+
+    <?php include 'php/menu.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="box">
+                    <div class="box-content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam et placeat porro itaque nulla voluptates nostrum officia? Architecto nihil laudantium tempore voluptatibus accusantium doloremque exercitationem aut quos, animi possimus.</p>
+                    </div>
+                </div>          
+            </div>
+            <div class="col">
+                <div class="box">
+                    <div class="box-content">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quam et placeat porro itaque nulla voluptates nostrum officia? Architecto nihil laudantium tempore voluptatibus accusantium doloremque exercitationem aut quos, animi possimus.</p>
+                    </div>
+                </div>          
+            </div>
+        </div>
+    </div>
+</body>
+
+
+</html>
