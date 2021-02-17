c22cdd6d7df7da95fccc5d17fd6e8043c5f98f72
SpectralX <galaxies20122@gmail.com>
2021-02-17 14:50:12 +0100

    teszt

--- app/Views/Shared/_layouter.php
+++
@@ -1,4 +0,0 @@
-<?= $this->extend($__layout) ?>
-<?= $this->section('content') ?>
-<?= $this->include($__view) ?>
-<?= $this->endSection() ?>
---
+++ php/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regisztráció</title>
+</head>
+
+<body>
+    <div class="container">
+    <form action="" method="POST" class="form_login margin-center">
+        <div>
+            <label>Felhasználó név</label>
+            <input type="text" name="username" value="" />
+        </div>
+
+        <div>
+            <label>Jelszó</label>
+            <input type="password" name="password" value="" />
+        </div>
+
+        <div class="buttons">
+            <input type="submit" name="submit" value="Bejelentkezés" />
+        </div>
+    </form>
+    </div>
+</body>
+
+</html>
