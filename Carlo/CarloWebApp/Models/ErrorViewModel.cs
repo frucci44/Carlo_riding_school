c606e275cf28cb035f0b44e52c9c28cd3c413af6
atesz77 <34383667+atesz77@users.noreply.github.com>
2021-02-19 16:55:48 +0100

    app csere

---
+++ Carlo/CarloWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CarloWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
