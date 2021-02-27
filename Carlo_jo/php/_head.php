08105899cc7bf14c608d5afbc9335f994e23cb42
atesz77 <34383667+atesz77@users.noreply.github.com>
2021-02-27 17:36:18 +0100

    bootstrap/jquery

--- Carlo_jo/php/_head.php
+++ Carlo_jo/php/_head.php
@@ -8,10 +8,13 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet">
+    
     <link rel="stylesheet" href="<?= $mappa ?? ''?>../css/menu.css">
-    <link rel="stylesheet" href="<?= $mappa ?? ''?>../css/style.css">
+    <link rel="stylesheet" href="<?= $mappa ?? ''?>../lib/bootstrap/dist/css/bootstrap.min.css">
   
-    <script src="<?= $mappa ?? ''?>../js/menu.js?b"></script>
+    <script src="<?= $mappa ?? ''?>../lib/jquery/dist/jquery.min.js"></script>
+    <script src="<?= $mappa ?? ''?>../lib/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?= $mappa ?? ''?>../js/menu.js"></script>
   
   
   
